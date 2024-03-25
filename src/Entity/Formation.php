fce49bf88b4f1fe0c0b07217864f136cdde0c0cd
SodoyerKyllian <kyllian.sodoeyr@campus-la-chataigneraie.org>
2024-03-25 11:09:31 +0100

    base de données

--- src/Entity/Formation.php
+++ src/Entity/Formation.php
@@ -1,8 +1,10 @@
 <?php
+// src/Entity/Formation.php
 
 namespace App\Entity;
 
-use App\Repository\FormationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,16 @@ class Formation
      */
     private $FOR_LIBELLE;
 
+    /**
+     * @ORM\OneToMany(targetEntity="Etudiant", mappedBy="formation")
+     */
+    private $etudiants;
+
+    public function __construct()
+    {
+        $this->etudiants = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +47,36 @@ class Formation
     public function setFORLIBELLE(string $FOR_LIBELLE): self
     {
         $this->FOR_LIBELLE = $FOR_LIBELLE;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Etudiant[]
+     */
+    public function getEtudiants(): Collection
+    {
+        return $this->etudiants;
+    }
+
+    public function addEtudiant(Etudiant $etudiant): self
+    {
+        if (!$this->etudiants->contains($etudiant)) {
+            $this->etudiants[] = $etudiant;
+            $etudiant->setFormation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEtudiant(Etudiant $etudiant): self
+    {
+        if ($this->etudiants->removeElement($etudiant)) {
+            // set the owning side to null (unless already changed)
+            if ($etudiant->getFormation() === $this) {
+                $etudiant->setFormation(null);
+            }
+        }
 
         return $this;
     }
