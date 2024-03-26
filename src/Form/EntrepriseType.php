02a52aced4f2a8c3904d6f3809a7412b015f2955
SodoyerKyllian <kyllian.sodoeyr@campus-la-chataigneraie.org>
2024-03-26 09:29:42 +0100

    controller Crud

---
+++ src/Form/EntrepriseType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Entreprise;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EntrepriseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('ENT_NOM')
+            ->add('ENT_VILLE')
+            ->add('ENT_PAYS')
+            ->add('ENT_SPECIALITE')
+            ->add('ENT_ADRESSE')
+            ->add('ENT_CP')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Entreprise::class,
+        ]);
+    }
+}
