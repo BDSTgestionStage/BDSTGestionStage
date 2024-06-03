01d4b7712d6119ff40c5412b0d44bb798d9aaac7
Tom.B <88104750+TomB76@users.noreply.github.com>
2024-06-03 02:23:15 +0200

    role

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -11,18 +11,17 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Utilisateur;
-use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Console\Formatter\OutputFormatter;
+use App\Entity\Role; // Import the Role entity
 
 class LoginController extends AbstractController
 {
     private $session;
-    
+
     public function __construct(SessionInterface $session)
     {
         $this->session = $session;
     }
-    
+
     /**
      * @Route("/login", name="app_login")
      */
@@ -30,13 +29,14 @@ class LoginController extends AbstractController
     {
         // Traitement du formulaire
         if ($request->isMethod('POST')) {
+            // Récupérer l'identifiant et le mot de passe soumis dans le formulaire
             $identifiant = $request->request->get('identifiant');
             $password = $request->request->get('password');
 
             // Vérifier si l'utilisateur existe
-            $identifiant = $em->getRepository('App\Entity\Utilisateur')->findOneBy(['identifiant' => $identifiant]);
+            $utilisateur = $em->getRepository(Utilisateur::class)->findOneBy(['identifiant' => $identifiant]);
 
-            if (!$identifiant) {
+            if (!$utilisateur) {
                 $this->addFlash('error', 'Utilisateur introuvable.');
                 return $this->redirectToRoute('app_login');
             }
@@ -45,16 +45,22 @@ class LoginController extends AbstractController
             $hashedPasswordSubmitted = hash('sha256', $password);
 
             // Comparer les hachages des mots de passe
-            if ($identifiant->getUTIPassword() !== $hashedPasswordSubmitted) {
+            if ($utilisateur->getUTIPassword() !== $hashedPasswordSubmitted) {
                 // Mot de passe incorrect
                 $this->addFlash('error', 'Mot de passe incorrect.');
                 return $this->redirectToRoute('app_login');
-            }           
+            }
             // Créer un cookie pour enregistrer la connexion de l'utilisateur
-            $cookieid = new Cookie('user_id', $identifiant->getId(), time() + 3600 * 24 * 7);
 
-            $cookiemdp = new Cookie('user_token', $identifiant->getUTIPassword(), time() + 3600 * 24 * 7); // Cookie valable pendant 7 jours
+            // Créer un cookie pour enregistrer la connexion de l'utilisateur
+            $cookieid = new Cookie('user_id', $utilisateur->getId(), time() + 3600 * 24 * 7);
+            $cookiemdp = new Cookie('user_token', $utilisateur->getUTIPassword(), time() + 3600 * 24 * 7); // Cookie valable pendant 7 jours
             $cookieconnected = new Cookie('connected', true, time() + 3600 * 24 * 7);
+            $cookieRole = new Cookie('user_role', $utilisateur->getRole()->getROLLIBELLE(), time() + 3600 * 24 * 7); // Cookie valable pendant 7 jours
+
+
+
+
 
             
 
@@ -63,10 +69,11 @@ class LoginController extends AbstractController
             $response->headers->setCookie($cookieid);
             $response->headers->setCookie($cookiemdp);
             $response->headers->setCookie($cookieconnected);
-          
+            $response->headers->setCookie($cookieRole);
+
             $response->send();
             $this->addFlash('success', 'Connexion réussie.');
-            
+
             // Rediriger vers la page d'accueil ou une autre page
             return $this->redirectToRoute('liste');
         }
@@ -74,6 +81,7 @@ class LoginController extends AbstractController
         // Afficher le formulaire
         return $this->render('BDST_PageConnexion.html.twig');
     }
+
     // deconnection 
     /**
      * @Route("/logout", name="app_logout")
@@ -85,6 +93,7 @@ class LoginController extends AbstractController
         $response->headers->clearCookie('user_id');
         $response->headers->clearCookie('user_token');
         $response->headers->clearCookie('connected');
+        $response->headers->clearCookie('user_role');
         $response->send();
 
         // Rediriger vers la page de connexion
